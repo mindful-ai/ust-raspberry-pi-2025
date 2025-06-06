fcbda2de78e1c31972502040f2e4e3e6b31fd8af
mindful-ai <mindfullearningindia@outlook.com>
2025-06-06 09:45:01 +0530

    Delete day-17/mqtt_led_control_secure directory

--- day-17/mqtt_led_control_secure/common/config.py
+++
@@ -1,7 +0,0 @@
-# common/config.py
-
-BROKER = "your.mqtt.broker.com"
-PORT = 8883  # TLS Port
-USERNAME = "your_username"
-PASSWORD = "your_password"
-CA_CERT = "certs/ca.crt"  # Path to CA certificate
